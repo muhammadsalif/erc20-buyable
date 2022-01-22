339b0dabe95c93829868e066d422acb08e3d3efb
Muhammad Salif <muhammad.salif@yahoo.com>
2022-01-22 19:47:34 +0500

    minor fix.

--- ERC20.sol
+++ ERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 import "./IERC20.sol";
-import "./Safemath.sol";
+import "./libraries/Safemath.sol";
 
 contract MyToken is IERC20 {
     using SafeMath for uint256;
